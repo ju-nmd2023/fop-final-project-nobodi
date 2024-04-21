d809d7c826809249955132bfa48afbdc6518ec4a
AbdullrahmanTurk <149819429+AbdullrahmanTurk@users.noreply.github.com>
2024-04-21 17:33:05 +0200

    keyPressed

--- public/game.js
+++ public/game.js
@@ -1,14 +1,18 @@
 let gameObjects = [];
+let x = 300;
+let y = 200;
+let spaceshipYSpeed = 0;
+const spaceshipYSpeedIncrement = 5;
 
 function setup() {
     createCanvas(800, 800);
     setInterval(spawnUfo, 8000);
 }
  
-noStroke();
-
 function draw() {
     background (0, 10, 30);
+    spaceship();
+    jets();
     
     for (let i = gameObjects.length - 1; i >= 0; i--) {
         gameObjects[i].display();
@@ -18,6 +22,11 @@ function draw() {
             gameObjects.splice(i, 1);
         }
     }
+    
+    // Update spaceship position
+    y += spaceshipYSpeed;
+    // Keep the spaceship within the canvas bounds
+    y = constrain(y, 75, height - 75);
 }
 
 function spawnUfo() {
@@ -57,20 +66,65 @@ class Ufo {
             this.timer++;
         }
     }
-
 }
 
-class Spa {
-    constructor() {
+function spaceship() {
+    push();
+    fill(20, 0, 220);
+    quad(x, y-75, x, y+75, x-75, y+50, x-75, y-50);
+    quad(x, y-75, x, y+75, x+200, y+25, x+200, y-25);
+    rect(x, y-130, 30, 80);
+    rect(x, y+50, 30, 80);
+    rect(x-75, y-150, 250, 50);
+    rect(x-75, y+100, 250, 50);
+    fill(0, 200, 255);
+    ellipse(x+100, y, 100, 50);
+    pop();
+}
 
+function jets() {
+    // yellow
+    push();
+    fill(255, 255, 0);
+    beginShape();
+    vertex(x-80, y+40);
+    bezierVertex(x-80, y+40, x-500, y, x-80, y-40);
+    endShape();
+    beginShape();
+    vertex(x-80, y-110);
+    bezierVertex(x-80, y-110, x-400, y-125, x-80, y-140);
+    endShape();
+    beginShape();
+    vertex(x-80, y+110);
+    bezierVertex(x-80, y+110, x-400, y+125, x-80, y+140);
+    endShape();
+    pop();
+    // orange
+    fill(255, 180, 0);
+    beginShape();
+    vertex(x-80, y+30);
+    bezierVertex(x-80, y+30, x-300, y, x-80, y-30);
+    endShape();
+    beginShape();
+    vertex(x-80, y-115);
+    bezierVertex(x-80, y-115, x-250, y-125, x-80, y-135);
+    endShape();
+    beginShape();
+    vertex(x-80, y+115);
+    bezierVertex(x-80, y+115, x-250, y+125, x-80, y+135);
+    endShape();
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        spaceshipYSpeed -= spaceshipYSpeedIncrement;
+    } else if (keyCode === DOWN_ARROW) {
+        spaceshipYSpeed += spaceshipYSpeedIncrement;
     }
+}
 
-    display() {
-
+function keyReleased() {
+    if (keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+        spaceshipYSpeed = 0;
     }
-
-    move() {
-        
-    }
-
 }
