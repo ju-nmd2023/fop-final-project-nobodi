2b53d642e95ce2779e49187e3b3bc96cc10df20f
Noah Buchanan <94912301+Buick142@users.noreply.github.com>
2024-05-13 13:30:15 +0200

    Update bosslevel.js

---
+++ public/bosslevel.js
@@ -0,0 +1,12 @@
+function ufo() {
+    fill(200, 0, 0);
+    arc(x, y, 400, 200, PI, PI + PI, PIE);
+    fill(0, 200, 255);
+    arc(x, y-80, 200, 200, PI, PI + PI, PIE);
+    fill(255, 255, 0);
+    ellipse(x, y-40, 40);
+    ellipse(x-75, y-40, 40);
+    ellipse(x-140, y-40, 40);
+    ellipse(x+75, y-40, 40);
+    ellipse(x+140, y-40, 40);
+}
